929e4e5a438aa7cd038506a2d4978bbabb095473
Daniel <danielfbest@hotmail.com>
2024-11-18 12:11:48 +0000

    Started project.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
 	"""The home page for Learning Log."""
@@ -21,3 +22,18 @@ def topic(request, topic_id):
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
 	
+def new_topic(request):
+	"""Add a new topic."""
+	if request.method != 'POST':
+		# No data submitted; create a blank form. 
+		form = TopicForm()
+	else:
+		# POST data submitted; process data. 
+		form = TopicForm(data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('learning_logs:topics')
+    
+    # Display a blank of invalid form. 
+	context = {'form': form}
+	return render(request, 'learning_logs/new_topic.html', context)
