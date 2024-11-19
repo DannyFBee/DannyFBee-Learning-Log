851e2f75c227e0e887e8293c56d75dfc4bbc80b2
Daniel <danielfbest@hotmail.com>
2024-11-19 13:22:50 +0000

    Chapter 19 PCC complete commit

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+"""Defines URL patterns for users."""
+
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+	# Include default auth urls. 
+	path('users/', include('django.contrib.auth.urls')),
+	# Registration page.
+	path('register/', views.register, name='register'),
+	# Logout page attempt.
+	path('logout/', views.logout_view, name='logout'),
+]
